56053e5017114ab3afec93695333cd5a92cbc540
BorkoAXT <79717574+BorkoAXT@users.noreply.github.com>
2024-12-09 18:06:12 +0200

    Add files via upload

---
+++ Card_Battles1/Card_Battles1/Card.cs
@@ -0,0 +1,49 @@
+﻿using GameOfWar;
+
+public class Card
+{
+    public CardFace Face { get; set; }
+    public CardSuit Suite { get; set; }
+
+
+    private static readonly Dictionary<CardSuit, string> SuitSymbols = new Dictionary<CardSuit, string>
+    {
+        { CardSuit.Spade, "♠" },
+        { CardSuit.Club, "♣" },
+        { CardSuit.Heart, "♥" },
+        { CardSuit.Diamond, "♦" }
+    };
+
+    public override string ToString()
+    {
+        string faceValue = GetFaceValue(Face);
+        string suitSymbol = SuitSymbols[Suite];
+
+        
+        string card = $@"
+  +-----+
+  |    {suitSymbol}|  
+  |  {faceValue,-2} |  
+  |{suitSymbol}    |  
+  +-----+";
+
+        return card;
+    }
+
+    private string GetFaceValue(CardFace face)
+    {
+        switch (face)
+        {
+            case CardFace.Ace:
+                return "A";
+            case CardFace.Jack:
+                return "J";
+            case CardFace.Queen:
+                return "Q";
+            case CardFace.King:
+                return "K";
+            default:
+                return ((int)face).ToString();
+        }
+    }
+}
