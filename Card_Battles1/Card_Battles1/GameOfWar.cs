c6cc9912cc08f1f93cb356f64bfec44607a58f0a
BorkoAXT <79717574+BorkoAXT@users.noreply.github.com>
2024-12-10 17:38:36 +0200

    Add files via upload

---
+++ Card_Battles1/Card_Battles1/GameOfWar.cs
@@ -0,0 +1,113 @@
+﻿using GameOfWar;
+using System.Media;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine(@"
+================================================================================
+|| Welcome to the Game of War!                                                ||
+||                                                                            ||
+|| HOW TO PLAY:                                                               ||
+|| + Each of the two players are dealt one half of a shuffled deck of cards.  ||
+|| + Each turn, each player draws one card from their deck.                   ||
+|| + The player that drew the card with higher value gets both cards.         ||
+|| + Both cards return to the winners deck.                                   ||
+|| + If there is a draw, both players place the next three cards face down    ||
+|| and then another card face-up. The owner of the higher face-up             ||
+|| card gets all the cards on the table.                                      ||
+||                                                                            ||
+|| HOW TO WIN:                                                                ||
+|| + The player who collects all the cards wins.                              ||
+||                                                                            ||
+|| CONTROLS:                                                                  ||
+|| + Press [Enter] to draw a new card until we have a winner.                 ||
+|| + Type 'help' to receive available comands                                 ||
+||                                                                            ||
+|| Have fun!                                                                  ||
+================================================================================");
+while (true)
+{
+    SoundPlayer keygen = new SoundPlayer(@"C:\Users\borko\Downloads\Keygen.wav");
+    SoundPlayer fear = new SoundPlayer(@"C:\Users\borko\Downloads\Fear.wav");
+    SoundPlayer goreNest = new SoundPlayer(@"C:\Users\borko\Downloads\SuperGoreMaze.wav");
+    SoundPlayer CastleVein = new SoundPlayer(@"C:\Users\borko\Downloads\CastleVein.wav");
+    SoundPlayer divineIntervation = new SoundPlayer(@"C:\Users\borko\Downloads\Divine Intervention.wav");
+    SoundPlayer wethands = new SoundPlayer(@"C:\Users\borko\Downloads\WetHands.wav");
+    SoundPlayer thickofIt = new SoundPlayer(@"C:\Users\borko\Downloads\ThickOfIt.wav");
+    SoundPlayer nothing = new SoundPlayer(@"C:\Users\borko\Downloads\nothing.wav");
+    Methods gameLogic = new Methods();
+
+    List<Card> deck = gameLogic.GenerateDeck();
+
+    gameLogic.ShuffleDeck(ref deck);
+
+    Queue<Card> firstPlayerDeck = new Queue<Card>();
+    Queue<Card> secondPlayerDeck = new Queue<Card>();
+
+    gameLogic.DealCardsToPlayers(ref deck, ref firstPlayerDeck, ref secondPlayerDeck);
+
+    Card firstPlayerCard = null;
+    Card secondPlayerCard = null;
+
+    int Moves = 0;
+
+    while (!gameLogic.GameHasWinner(ref firstPlayerDeck, ref secondPlayerDeck, ref Moves))
+    {
+        
+        string command = Console.ReadLine();
+        if (command.Equals("Win", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("You have chosen to simply win. Congratulations!");
+            firstPlayerDeck = new Queue<Card>(gameLogic.GenerateDeck());
+            secondPlayerDeck.Clear();
+            break;
+        }
+        if (command.Equals("Lose", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("You have chosen to lose! Better luck next time.");
+            firstPlayerDeck.Clear();
+            secondPlayerDeck = new Queue<Card>(gameLogic.GenerateDeck());
+            break;
+        }
+        if (command.Equals("settings", StringComparison.OrdinalIgnoreCase))
+        {
+            gameLogic.OpenSettingsMenu(keygen, divineIntervation, CastleVein, fear, goreNest, thickofIt, nothing, wethands);
+            continue;
+        }
+        if (command.Equals("help", StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("type 'Win' to win the game");
+            Console.WriteLine("type 'Lose' to lose the game");
+            Console.WriteLine("Type 'settings' to open the settings menu, or press [Enter] to continue.");
+            continue;
+        }
+
+        
+        Console.Clear();
+
+        gameLogic.DrawPlayersCards(ref firstPlayerCard, ref secondPlayerCard, ref firstPlayerDeck, ref secondPlayerDeck);
+
+        Queue<Card> pool = new Queue<Card>();
+
+        pool.Enqueue(firstPlayerCard);
+        pool.Enqueue(secondPlayerCard);
+
+        gameLogic.ProcessWar(ref pool, ref firstPlayerCard, ref secondPlayerCard, ref firstPlayerDeck, ref secondPlayerDeck);
+        gameLogic.DetermineRoundWinner(pool, ref firstPlayerCard, ref secondPlayerCard, ref firstPlayerDeck, ref secondPlayerDeck);
+
+        Console.WriteLine("==================================================================");
+        Console.WriteLine($"First player currently has {firstPlayerDeck.Count} cards.");
+        Console.WriteLine($"Second player currently has {secondPlayerDeck.Count} cards.");
+        Console.WriteLine("==================================================================");
+        Moves++;
+    }
+    
+
+    Console.WriteLine("Do you want to play another game? yes/no");
+    if (!Console.ReadLine().Equals(("yes").ToLower(), StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("Thank you for playing! Goodbye!");
+        break;
+    }
+    Console.Clear();
+}
---
+++ Card_Battles1/Card_Battles1/Methods.cs
@@ -0,0 +1,315 @@
+﻿using System.Media;
+namespace GameOfWar
+
+{
+    public class Methods
+    {
+
+
+        public void ShuffleDeck(ref List<Card> deck)
+        {
+            Random random = new Random();
+            for (int i = 0; i < deck.Count; i++)
+            {
+                int firstCardIndex = random.Next(deck.Count);
+
+                Card tempCard = deck[firstCardIndex];
+                deck[firstCardIndex] = deck[i];
+                deck[i] = tempCard;
+            }
+        }
+
+        public void DealCardsToPlayers(ref List<Card> deck, ref Queue<Card> firstPlayerDeck, ref Queue<Card> secondPlayerDeck)
+        {
+            bool toFirst = true;
+            while (deck.Any())
+            {
+                if (toFirst)
+                {
+                    firstPlayerDeck.Enqueue(deck[0]);
+                }
+                else
+                {
+                    secondPlayerDeck.Enqueue(deck[0]);
+                }
+
+                deck.RemoveAt(0);
+                toFirst = !toFirst;
+            }
+        }
+
+
+        public bool GameHasWinner(ref Queue<Card> firstDeck, ref Queue<Card> secondDeck, ref int totalMoves)
+        {
+            if (firstDeck.Count < 4)
+            {
+                Console.WriteLine($"After a total of {totalMoves} moves, the second player has won!");
+                return true;
+            }
+            if (secondDeck.Count < 4)
+            {
+                Console.WriteLine($"After a total of {totalMoves} moves, the first player has won!");
+                return true;
+            }
+            return false;
+
+        }
+
+        public void DrawPlayersCards(ref Card firstPlayerCard, ref Card secondPlayerCard, ref Queue<Card> firstPlayerDeck, ref Queue<Card> secondPlayerDeck)
+        {
+            firstPlayerCard = firstPlayerDeck.Dequeue();
+            Console.WriteLine($"First player has drawn: {firstPlayerCard}");
+            secondPlayerCard = secondPlayerDeck.Dequeue();
+            Console.WriteLine($"Second player has drawn: {secondPlayerCard}");
+
+        }
+
+        public void AddCardsToWinnerDeck(Queue<Card> loserDeck, Queue<Card> winner)
+        {
+            while (loserDeck.Count > 0)
+            {
+                winner.Enqueue(loserDeck.Dequeue());
+            }
+        }
+
+        public void AddWarCardsToPool(Queue<Card> pool, ref Queue<Card> firstPlayerDeck, ref Queue<Card> secondPlayerDeck)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                pool.Enqueue(firstPlayerDeck.Dequeue());
+                pool.Enqueue(secondPlayerDeck.Dequeue());
+            }
+        }
+
+        public void DetermineRoundWinner(Queue<Card> allCards, ref Card firstPlayerCard, ref Card secondPlayerCard, ref Queue<Card> firstPlayerDeck, ref Queue<Card> secondPlayerDeck)
+        {
+            if ((int)firstPlayerCard.Face > (int)secondPlayerCard.Face)
+            {
+                Console.WriteLine("The first player has won the cards!");
+
+                foreach (var card in allCards)
+                {
+                    firstPlayerDeck.Enqueue(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The second player has won the cards!");
+
+                foreach (var card in allCards)
+                {
+                    secondPlayerDeck.Enqueue(card);
+                }
+            }
+        }
+                
+        
+        public List<Card> GenerateDeck()
+        {
+            List<Card> deck = new List<Card>();
+            CardFace[] faces = (CardFace[])Enum.GetValues(typeof(CardFace));
+            CardSuit[] suits = (CardSuit[])Enum.GetValues(typeof(CardSuit));
+
+            for (int suite = 0; suite < suits.Length; suite++)
+            {
+                for (int face = 0; face < faces.Length; face++)
+                {
+                    CardFace currentFace = faces[face];
+                    CardSuit currentSuit = suits[suite];
+                    deck.Add(new Card
+                    {
+                        Face = currentFace,
+                        Suite = currentSuit
+
+                    }
+                    );
+                }
+            }
+            return deck;
+        }
+
+        public void ProcessWar(ref Queue<Card> pool, ref Card firstPlayerCard, ref Card secondPlayerCard, ref Queue<Card> firstPlayerDeck, ref Queue<Card> secondPlayerDeck)
+        {
+            while ((int)firstPlayerCard.Face == (int)secondPlayerCard.Face)
+            {
+                Console.WriteLine("WAR! ");
+
+                if (firstPlayerDeck.Count < 4)
+                {
+                    Console.WriteLine("First player does not have enough cards to continue. Second player wins the game!");
+                    secondPlayerDeck = new Queue<Card>(secondPlayerDeck.Concat(pool));
+                    firstPlayerDeck.Clear();
+                    return;
+                }
+
+                if (secondPlayerDeck.Count < 4)
+                {
+                    Console.WriteLine("Second player does not have enough cards to continue. First player wins the game!");
+                    firstPlayerDeck = new Queue<Card>(firstPlayerDeck.Concat(pool));
+                    secondPlayerDeck.Clear();
+                    return;
+                }
+
+                AddWarCardsToPool(pool, ref firstPlayerDeck, ref secondPlayerDeck);
+
+                firstPlayerCard = firstPlayerDeck.Dequeue();
+                secondPlayerCard = secondPlayerDeck.Dequeue();
+
+                Console.WriteLine($"First player has drawn: {firstPlayerCard}");
+                Console.WriteLine($"Second player has drawn: {secondPlayerCard}");
+
+                pool.Enqueue(firstPlayerCard);
+                pool.Enqueue(secondPlayerCard);
+            }
+        }
+        
+        
+            public void OpenSettingsMenu(SoundPlayer keygen, SoundPlayer divineIntervation, SoundPlayer CastleVein, SoundPlayer fear, SoundPlayer goreNest, SoundPlayer thickofIt, SoundPlayer nothing, SoundPlayer wethands)
+            {
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Settings Menu:");
+                    Console.WriteLine("1. Change Song");
+                    Console.WriteLine("2. Change Background Color");
+                    Console.WriteLine("3. Exit Settings");
+                    Console.Write("Enter your choice: ");
+                    string choice = Console.ReadLine();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            ChangeSong(keygen, divineIntervation, CastleVein, fear, goreNest, thickofIt, nothing, wethands);
+                            break;
+
+                        case "2":
+                            ChangeBackgroundColor(keygen, divineIntervation, CastleVein, fear, goreNest, thickofIt, nothing, wethands);
+                            break;
+
+                        case "3":
+                            Console.Clear();
+                            return;
+
+                        default:
+                            Console.WriteLine("Invalid choice, please try again.");
+                        Thread.Sleep(1000);
+                            break;
+                    }
+                }
+            }
+
+            public void ChangeSong(SoundPlayer keygen, SoundPlayer divineIntervation, SoundPlayer CastleVein, SoundPlayer fear, SoundPlayer goreNest, SoundPlayer thickofIt, SoundPlayer nothing, SoundPlayer wethands)
+            {
+                Console.Clear();
+                Console.WriteLine("Select a song:");
+                Console.WriteLine("1. ULTRAKILL: Keygen Church");
+                Console.WriteLine("2. ULTRAKILL: Divine Intervention");
+                Console.WriteLine("3. ULTRAKILL: Castle Vein");
+                Console.WriteLine("4. DOOM: The Only Thing They Fear Is You");
+                Console.WriteLine("5. DOOM: The Super Gore Nest");
+                Console.WriteLine("6. KSI: Thick of It");
+                Console.WriteLine("7. Minecraft: Wet Hands");
+                Console.WriteLine("8. None");
+                Console.WriteLine();
+                Console.WriteLine("type 'back' to go back.");
+
+                string command = Console.ReadLine();
+
+            if (command == "1")
+            {
+                keygen.Play();
+            }
+            else if (command == "2")
+            {
+                divineIntervation.Play();
+            }
+            else if (command == "3")
+            {
+                CastleVein.Play();
+            }
+            else if (command == "4")
+            {
+                fear.Play();
+            }
+            else if (command == "5")
+            {
+                goreNest.Play();
+            }
+            else if (command == "6") {
+                thickofIt.Play();
+            }
+            else if (command == "7")
+            {
+                wethands.Play();
+            }
+            else if (command == "8")
+            {
+                nothing.Play();
+            }
+            else if (command == "back") { 
+                OpenSettingsMenu(keygen, divineIntervation, CastleVein, fear, goreNest, thickofIt, nothing, wethands);
+                Thread.Sleep(0);
+            }
+            else
+            {
+                
+                Console.WriteLine("Invalid selection");
+                Thread.Sleep(1000);
+                ChangeSong(keygen, divineIntervation, CastleVein, fear, goreNest, thickofIt, nothing, wethands);
+            }
+            
+             
+            
+        }
+
+            public void ChangeBackgroundColor(SoundPlayer keygen, SoundPlayer divineIntervation, SoundPlayer CastleVein, SoundPlayer fear, SoundPlayer goreNest, SoundPlayer thickofIt, SoundPlayer nothing, SoundPlayer wethands)
+            {
+                Console.Clear();
+                Console.WriteLine("Select a background color:");
+                Console.WriteLine("1. Black");
+                Console.WriteLine("2. Blue");
+                Console.WriteLine("3. Green");
+                Console.WriteLine("4. Red");
+                Console.WriteLine("5. Yellow");
+                Console.WriteLine();
+                Console.WriteLine("type 'back' to go back.");
+
+                string colorChoice = Console.ReadLine();
+
+                switch (colorChoice)
+                {
+                    case "1":
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    case "2":
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        break;
+                    case "3":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        break;
+                    case "4":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        break;
+                    case "5":
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        break;
+                    case "back":
+                    OpenSettingsMenu(keygen, divineIntervation, CastleVein, fear, goreNest, thickofIt, nothing, wethands);
+                    Thread.Sleep(0);
+                    break;
+                    default:
+                        Console.WriteLine("Invalid color selection!");
+
+                        
+                    Thread.Sleep(1000);
+                    ChangeBackgroundColor(keygen, divineIntervation, CastleVein, fear, goreNest, thickofIt, nothing, wethands);
+
+                    break;
+                }
+
+                Console.Clear(); 
+            }
+        }
+
+
+    }
